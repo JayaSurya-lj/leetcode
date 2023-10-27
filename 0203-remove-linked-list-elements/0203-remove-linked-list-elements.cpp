b2bafc2cee822ac23101447273c5bf064fafe332
JayaSurya-lj <129645016+JayaSurya-lj@users.noreply.github.com>
2023-10-27 17:23:03 +0530

    Time: 20 ms (25.38%), Space: 15.1 MB (77.27%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -10,63 +10,24 @@
  */
 class Solution {
 public:
-    int size(ListNode* head)
-    {
-        if(head==NULL) return 0;
-        int c = 0;
-        ListNode* temp = head;
-        while(temp!=NULL)
-        {
-            c++;
-            temp = temp->next;
-        }
-
-        return c;
-    }
-    ListNode* deleteNode(ListNode* head,int idx)
-    {
-        ListNode* temp = head;
-        int length = size(head);
-        if(idx==0)
-        {
-            head = head->next;
-            return head;
-        }
-        if(idx==length-1)
-        {
-            for(int i=1;i<=idx-1;i++)
-            {
-                temp = temp->next;
-            }
-            temp->next = NULL;
-            return head;
-        }
-
-        for(int i=1;i<=idx-1;i++)
-        {
-            temp = temp->next;
-        }
-        temp->next = temp->next->next;
-        return head;
-    }
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* temp = head;
-        int idx = 0;
-        while(temp!=NULL)
+        ListNode* curr = head;
+        while(curr and curr->val==val)
         {
-            bool flag = true;
-            if(temp->val==val)
-            {
-                head = deleteNode(head,idx);
-                flag = false;
-                temp = head;
-                idx = 0;
-            }
+            curr = curr->next;
+        }
+        head = curr;
 
-            if(flag==true)
-            idx++;
-            if(flag==true)
-            temp = temp->next;
+        while(curr)
+        {
+            if(curr->next and curr->next->val==val)
+            {
+                curr->next = curr->next->next;
+            }
+            else
+            {
+                curr = curr->next;
+            }
         }
         return head;
         
