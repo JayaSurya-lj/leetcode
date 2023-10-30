393baad2a3c081a26b6bbc62536ab47739ea2ead
JayaSurya-lj <129645016+JayaSurya-lj@users.noreply.github.com>
2023-10-30 23:53:42 +0530

    Time: 10 ms (22.84%), Space: 11.5 MB (27.01%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -28,34 +28,48 @@ public:
             t = t->next;
             
         }
-        temp = head;
-        t = dummy->next;
-        while(temp!=NULL && t!=NULL)
+        Node* t1 = head;
+        Node* t2 = dummy->next;
+        dummy = new Node(100);
+        Node* td = dummy;
+        while(t1!=NULL && t2!=NULL)
         {
-            Node* a = temp->random;
-            if(a==NULL)
-            {
-                t->random = NULL;
-                temp = temp->next;
-                t = t->next;
-                continue;
-            }
-            Node* a1 = head;
-            Node* a2 = dummy->next;
-            while(a1!=NULL)
-            {
-                if(a1==a) break;
-                a1 = a1->next;
-                a2 = a2->next;
-            }
+            td->next = t1;
+            t1 = t1->next;
+            td = td->next;
+            td->next = t2;
+            t2 = t2->next;
+            td = td->next;
 
-            t->random = a2;
-
-            temp = temp->next;
-            t = t->next;
         }
-        return dummy->next;
+        Node* merge = dummy->next;
+        t1 = dummy->next;
+        while(t1!=NULL)
+        {
+            t2 = t1->next;
+            if(t1->random!=NULL) t2->random = t1->random->next;
+            t1 = t1->next->next;
+        }
 
-        
+        t1 = merge;
+        Node* dummy1 = new Node(100);
+        Node* td1 = dummy1;
+        Node* dummy2 = new Node(100);
+        Node* td2 = dummy2;
+
+        while(t1!=NULL)
+        {
+            td1->next = t1;
+            t1 = t1->next;
+            td1 = td1->next;
+
+            td2->next = t1;
+            t1 = t1->next;
+            td2 = td2->next;
+
+
+        }
+        td1->next = NULL;
+        return dummy2->next;
     }
 };
