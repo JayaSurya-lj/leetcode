348bef5433342853688a5ef17769ac07322640a2
JayaSurya-lj <129645016+JayaSurya-lj@users.noreply.github.com>
2024-01-12 09:33:41 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (40.00%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,12 +16,6 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
         if(nums.size()==2) return max(nums[0],nums[1]);
-        if(nums.size()==3){
-            int test = max(max(nums[0],nums[1]),nums[2]);
-            int a = nums[0] + nums[2];
-            int b = max(a,nums[1]);
-            return max(test,b);
-        }
         vector<int> mem(nums.size());
         mem[nums.size()-1] = nums[nums.size()-1];
         mem[nums.size()-2] = nums[nums.size()-2];
